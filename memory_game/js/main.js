446d57fdf57ec1ecc8d866bef826e92c8c717b06
Bhandarkar Roopali <BhandarkarRoopaliT@JohnDeere.com>
2019-11-13 10:31:26 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king" , "king"];
+var cardsInPlay = [];
+var cardOne = 'queen';
+cardsInPlay.push('cardOne');
+var cardTwo = 'king';
+cardsInPlay.push('cardTwo');
+
+var cardThree = 'king';
+cardsInPlay.push('cardTwo');
+
+console.log(cardsInPlay);
+
+if(cardsInPlay.length === 3){ 
+	if (cardsInPlay[1] === cardsInPlay[2]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again");
+	}
+ }
+
+
+
+ 
+
+
