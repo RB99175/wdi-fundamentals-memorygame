adf786585231269b883047869fbd1443cb8a6b40
Bhandarkar Roopali <BhandarkarRoopaliT@JohnDeere.com>
2019-11-13 20:29:07 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,32 +1,62 @@
-console.log("Up and running!");
-var cards = ["queen", "queen", "king" , "king"];
-var cardsInPlay = [];
 
+console.log("Up and running!");
+// var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 console.log(cardsInPlay);
 
-function checkForMatch(){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
-} else {
-  console.log("Sorry, try again.");
-}
+// This is Array Object 
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images / queen - of - hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamand",
+		cardImage: "images / queen - of - dimonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images / king - of - hearts.png"
+	},
 
-}
-
-function flipCard(cardId){
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	if(cardsInPlay.length === 3){ 
-		checkForMatch();
-	if (cardsInPlay[1] === cardsInPlay[2]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images / king - of - dimonds.png"
 	}
- }
 
+];
 
+// this is function 
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+
+function flipCard(cardId) {
+	// Display Rank 
+	console.log("User flipped--" + cards[cardId].rank);
+	// Display Image 
+	console.log("Image --" + cards[cardId].cardImage);
+	//	Display Suit 
+	console.log("suit--" + cards[cardId].suit);
+
+	if (cardsInPlay.length === 3) {
+		checkForMatch();
+		if (cardsInPlay[1] === cardsInPlay[2]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again");
+		}
+	}
 }
 
 flipCard(0);
-flipCard(2);
+flipCard(2); 
+ 
