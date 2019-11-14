edb0f0238cabb0231022053b8973559fdec28e4d
Bhandarkar Roopali <BhandarkarRoopaliT@JohnDeere.com>
2019-11-14 11:00:17 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -9,28 +9,38 @@ const cards = [
 	{
 		rank: "queen",
 		suit: "hearts",
-		cardImage: "images / queen - of - hearts.png"
+		cardImage: "images/queen-of-hearts.png"
 	},
 	{
 		rank: "queen",
-		suit: "diamand",
-		cardImage: "images / queen - of - dimonds.png"
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
 	},
 	{
 		rank: "king",
 		suit: "hearts",
-		cardImage: "images / king - of - hearts.png"
+		cardImage: "images/king-of-hearts.png"
 	},
 
 	{
 		rank: "king",
 		suit: "diamonds",
-		cardImage: "images / king - of - dimonds.png"
+		cardImage: "images/king-of-diamonds.png"
 	}
 
 ];
 
-// this is function 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+} 
+
+
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
@@ -39,17 +49,18 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-	// Display Rank 
+function flipCard() {
+	var cardId = this.getAttribute('data-id');	
 	console.log("User flipped--" + cards[cardId].rank);
-	// Display Image 
 	console.log("Image --" + cards[cardId].cardImage);
-	//	Display Suit 
 	console.log("suit--" + cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 
-	if (cardsInPlay.length === 3) {
+	if (cardsInPlay.length === 2) {
 		checkForMatch();
-		if (cardsInPlay[1] === cardsInPlay[2]) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("Match Found!")
 			alert("You found a match!");
 		} else {
 			alert("Sorry, try again");
@@ -57,6 +68,6 @@ function flipCard(cardId) {
 	}
 }
 
-flipCard(0);
-flipCard(2); 
+createBoard();
+
  
